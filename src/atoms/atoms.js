840b38cd55852cc1d251f9b8bd4ea0c297beeb93
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-05-15 22:54:39 +0200

    restructure

--- src/atoms/atoms.js
+++ src/atoms/atoms.js
@@ -1,4 +1,5 @@
 import { atom, selector } from "recoil";
+import { createArray, modifyArray, reducer } from "../shared/common";
 
 export const currentLetter = atom({
   key: "currentLetter",
@@ -38,14 +39,6 @@ export const letterIndex = selector({
   },
 });
 
-const createArray = () => {
-  const matrix = new Array(5)
-    .fill(0)
-    .map(() => new Array(5).fill({ letter: "", color: "slate" }));
-  console.log(matrix);
-  return matrix;
-};
-
 export const letters = atom({
   key: "letters",
   default: createArray(),
@@ -69,74 +62,27 @@ export const gussedLetters = selector({
   },
 });
 
+export const currentBgColor = atom({
+  key: "bgcolor",
+  default: "bg-slate-500",
+});
+
 export const colorLetters = selector({
   key: "colorLetters",
   get: ({ get }) => {
     return get(letters);
   },
   set: ({ get, set }, index) => {
+    console.log(get(currentBgColor));
     set(letters, (state) =>
-      modifyArray(state, get(currentWordIndex), index, "", "color")
+      modifyArray(
+        state,
+        get(currentWordIndex),
+        index,
+        "",
+        "color",
+        get(currentBgColor)
+      )
     );
   },
 });
-
-const reducer = (value, action) => {
-  switch (action) {
-    case "increase":
-      value = limitValue((value += 1));
-      break;
-    case "decrease":
-      value = limitValue((value -= 1));
-      break;
-    case "zero":
-      value = 0;
-      break;
-    default:
-      break;
-  }
-  console.log(value);
-  return value;
-};
-
-const modifyArray = (arr, y, x, value, action) => {
-  var newArr = arr;
-  switch (action) {
-    case "add":
-      if (arr[y][x].letter === "") {
-        var word = [...arr[y]];
-        var obj = { ...word[x] };
-        obj.letter = value;
-        word = replaceItemAtIndex([...word], x, obj);
-        newArr = replaceItemAtIndex([...arr], y, word);
-      }
-      break;
-    case "delete":
-      word = [...arr[y]];
-      obj = { ...word[x] };
-      obj.letter = "";
-      word = deleteItemAtIndex([...word], x, obj);
-      newArr = deleteItemAtIndex([...arr], y, word);
-      break;
-    case "color":
-        console.log("color");
-      break;
-    default:
-      newArr = arr;
-      break;
-  }
-  console.log(newArr);
-  return newArr;
-};
-
-const limitValue = (val) => {
-  return val >= 4 ? 4 : val <= 0 ? 0 : val;
-};
-
-const replaceItemAtIndex = (arr, index, newValue) => {
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-};
-
-const deleteItemAtIndex = (arr, index, newValue) => {
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-};
