18509beb2322b52fc110f2d730909f21a4f7d52f
Håkan <hali0151@student.umu.se>
2022-05-18 15:05:51 +0200

    restructure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import Header from "./components/Header";
+import React, { useState } from 'react';
 import Game from "./components/Game";
-import Keyboard from "./components/Keyboard";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil";
+
+const correctWord = "HELLO";
 
 function App() {
+
   return (
     <RecoilRoot>
-      <div className="h-full bg-slate-600 flex flex-col divide-y divide-slate-900">
-        <Header />
-        <Game />
-        <Keyboard />
-      </div>
+      <Game />
     </RecoilRoot>
   );
 }
