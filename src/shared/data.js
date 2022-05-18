4a5b544264f8cdc3d935c32a46a05cb25b7eb4ef
Håkan <hali0151@student.umu.se>
2022-05-18 15:07:35 +0200

    exports colors and initial letters

---
+++ src/shared/data.js
@@ -0,0 +1,8 @@
+export const lettersList =
+"A,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,ENTER,Z,X,C,V,B,N,M,BACKSPACE";
+
+export const GREEN = 'bg-emerald-700';
+export const YELLOW = 'bg-amber-400';
+export const DARKGREY = 'bg-emerald-700';
+export const LIGHTGREY = 'bg-slate-700';
+
