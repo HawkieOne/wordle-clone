325a764470d853267c8dff60c7007a881601d108
Håkan <hali0151@student.umu.se>
2022-05-28 11:13:03 +0200

    add class for dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 import Game from "./components/Game";
 import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-toggle/style.css" 
 
 function App() {
-
   return (
     <RecoilRoot>
       <Game />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+      />
     </RecoilRoot>
   );
 }
