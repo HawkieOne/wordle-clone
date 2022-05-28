1942a86236bdf1d9f230274a1266dd7dd5fec09e
Håkan <hali0151@student.umu.se>
2022-05-28 11:12:43 +0200

    new config
    - custom colors
    - enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'whiteBG': '#E5E9F0',
+        'accent-dark': '#3B4252',
+        'letter-dark': '#81A1C1',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
