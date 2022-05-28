c7640fc7879129a12db335e73eec664db2245f23
Håkan <hali0151@student.umu.se>
2022-05-28 11:16:03 +0200

    add new const color

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,8 +1,9 @@
 export const lettersList =
-"A,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,ENTER,Z,X,C,V,B,N,M,BACKSPACE";
+"Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,ENTER,Z,X,C,V,B,N,M,BACKSPACE";
 
 export const GREEN = 'bg-emerald-700';
 export const YELLOW = 'bg-amber-400';
 export const DARKGREY = 'bg-slate-700';
 export const LIGHTGREY = 'bg-slate-500';
+export const GREY_DMODE = 'bg-letter-dark';
 
